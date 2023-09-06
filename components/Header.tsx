15b69da00fc8becd086abfd1837d4a6a1443f1da
E.J. Perez <ejperez37@gmail.com>
2023-09-06 10:16:09 -0400

    Create nav, create buttons

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,23 @@
 import Link from "next/link"
 import Container from "./ui/container"
+import { Button } from "./ui/button";
+import { Moon, ShoppingCart, Sun } from "lucide-react";
+
+//statically set for this project, would normally be imported from a database
+const routes = [
+    {
+        href: "/",
+        label: "Products",
+    },
+    {
+        href: "/",
+        label: "Categories",
+    },
+    {
+        href: "/",
+        label: "On Sale",
+    },
+];
 
 const Header = () => {
     return (
@@ -13,6 +31,32 @@ const Header = () => {
                             STORE NAME
                         </h1>
                     </Link>
+                </div>
+                <nav className="mx-6 flex items-center space-x-4 lg:space-x-6 hidden md:block">
+                    {routes.map((route, i) => (
+                        <Button asChild key={i} variant="ghost">
+                            <Link
+                                key={i}
+                                href={route.href}
+                                className="text-sm font-medium transition-colors"
+                            >
+                                    {route.label}
+                            </Link>
+                        </Button>
+                    ))}
+                </nav>
+                <div className ="flex items-center">
+                    <Button variant="ghost" size="icon" className="mr-2"
+                     aria-label="Shopping Cart">
+                        <ShoppingCart className="h-6 w-6" />
+                        <span className="sr-only">Shopping Cart</span>
+                    </Button>
+                    <Button variant="ghost" size="icon" aria-label="Toggle Theme"
+                     className="mr-6">
+                        <Sun className="h-6 w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                        <Moon className="absolute h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                        <span className="sr-only">Toggle Theme</span>
+                    </Button>
                 </div>
             </div>
             </Container>
